060809cd452572e29dbbdb203ddc45f546fc234e
Joalor64 <rhgarcon@gmail.com>
2022-11-01 09:16:18 -0400

    Create NGREState.hx

---
+++ source/NGREState.hx
@@ -0,0 +1,33 @@
+package;
+
+import Paths;
+import flixel.FlxG;
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.FlxState;
+
+class NGREState extends FlxState
+{
+    public var DisplayText:FlxText;
+
+    override function create()
+    {
+		super.create();
+
+                DisplayText = new FlxText(0, 0, FlxG.width, "This is unfinished.", 32);
+		DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+
+		add(DisplayText);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        if (FlxG.keys.justPressed.ANY)
+	{
+		FlxG.switchState(new MainMenuState());
+	}
+    }
+}
