7a42c4479617f470e59f99e52d4ffa95b880f97d
Joalor64 <rhgarcon@gmail.com>
2022-11-05 16:09:45 -0400

    Update InstructionsState.hx

--- source/InstructionsState.hx
+++ source/InstructionsState.hx
@@ -15,7 +15,7 @@ class InstructionsState extends FlxState
     {
 		super.create();
 
-    var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+                var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		bg.scrollFactor.x = 0;
 		bg.scrollFactor.y = 0.18;
 		bg.setGraphicSize(Std.int(bg.width * 1.1));
