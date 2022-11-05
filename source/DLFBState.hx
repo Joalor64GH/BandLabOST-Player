6b7490f818263202792311d7d4c635e6b9066b24
Joalor64 <rhgarcon@gmail.com>
2022-11-04 21:50:27 -0400

    backgrounds wip

--- source/DLFBState.hx
+++ source/DLFBState.hx
@@ -2,6 +2,7 @@ package;
 
 import Paths;
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
@@ -14,6 +15,15 @@ class DLFBState extends FlxState
     override function create()
     {
 		super.create();
+
+    var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('backgrounds/dreamylofibeats'));
+		bg.scrollFactor.x = 0;
+		bg.scrollFactor.y = 0.18;
+		bg.setGraphicSize(Std.int(bg.width * 1.1));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.antialiasing = true;
+		add(bg);
 
                 DisplayText = new FlxText(0, 0, FlxG.width, "NOW PLAYING:\nDreamy Lo-Fi Beats", 32);
 		DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
