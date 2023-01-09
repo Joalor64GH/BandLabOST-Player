8dc8d663c66cdc93d750d518822c7859153b7f2e
Joalor64 <rhgarcon@gmail.com>
2023-01-09 08:20:18 -0500

    trying to make softcoded file loading

--- source/Paths.hx
+++ source/Paths.hx
@@ -1,9 +1,12 @@
 package;
 
-import openfl.utils.Assets as OpenFlAssets;
-
 import flixel.FlxG;
+import openfl.utils.Assets as OpenFlAssets;
 import flixel.graphics.frames.FlxAtlasFrames;
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
 
 class Paths
 {
@@ -20,53 +23,118 @@ class Paths
 		return path;
 	}
 
+	#if MOD_SUPPORT
 	inline static public function fromModFolders(file:String)
 	{
 		return 'mods/$file';
 	}
+	#end
 	
 	inline static public function txt(key:String)
 	{
-		return file('data/$key.txt');
+		if(FileSystem.exists)
+		{
+            return fromModFolders('data/$key.txt');
+		}
+		else
+		{
+		    return file('data/$key.txt');
+		}	
 	}
 
         inline static public function xml(key:String)
 	{
-		return file('data/$key.xml');
+		if(FileSystem.exists)
+		{
+			return fromModFolders('data/$key.xml');
+		}
+		else
+		{
+		    return file('data/$key.xml');
+		}
 	}
 
 	inline static public function sound(key:String)
 	{
-		return file('sounds/$key.$SOUND_EXT');
+		if(FileSystem.exists)
+		{
+			return fromModFolders('sounds/$key.$SOUND_EXT');
+		}
+		else
+		{
+            return file('sounds/$key.$SOUND_EXT');
+		}
 	}
 
 	inline static public function soundRandom(key:String, min:Int, max:Int)
 	{
-		return file('sounds/$key${FlxG.random.int(min, max)}.$SOUND_EXT');
+		if(FileSystem.exists)
+		{
+            return fromModFolders('sounds/$key${FlxG.random.int(min, max)}.$SOUND_EXT');
+		}
+		else
+		{
+		    return file('sounds/$key${FlxG.random.int(min, max)}.$SOUND_EXT');
+		}
 	}
 
 	inline static public function music(key:String)
 	{
-		return file('music/$key.$SOUND_EXT');
+		if(FileSystem.exists)
+		{
+		    return fromModFolders('music/$key.$SOUND_EXT');
+		}
+		else
+		{
+			return file('music/$key.$SOUND_EXT');
+		}
 	}
 
 	inline static public function image(key:String)
 	{
-		return file('images/$key.png');
+		if(FileSystem.exists)
+		{
+		    return fromModFolders('images/$key.png');
+		}
+		else
+		{
+			return file('images/$key.png');
+		}
 	}
 
 	inline static public function font(key:String)
 	{
-		return file('fonts/$key');
+		if (FileSystem.exists)
+		{
+            return fromModFolders('fonts/$key');
+		}
+		else
+		{
+            return file('fonts/$key');
+		}
 	}
 
 	inline static public function getSparrowAtlas(key:String)
 	{
-		return FlxAtlasFrames.fromSparrow(image(key), file('images/$key.xml'));
+		if (FileSystem.exists)
+		{
+            return FlxAtlasFrames.fromSparrow(image(key), fromModFolders('images/$key.xml'));
+		}
+		else
+		{
+			return FlxAtlasFrames.fromSparrow(image(key), file('images/$key.xml'));
+		}
 	}
 
 	inline static public function getPackerAtlas(key:String)
 	{
-		return FlxAtlasFrames.fromSpriteSheetPacker(image(key), file('images/$key.txt'));
+		if (FileSystem.exists)
+		{
+            return FlxAtlasFrames.fromSpriteSheetPacker(image(key), fromModFolders('images/$key.txt'));
+		}
+		else
+		{
+			return FlxAtlasFrames.fromSpriteSheetPacker(image(key), file('images/$key.txt'));
+		}
 	}
 }  
