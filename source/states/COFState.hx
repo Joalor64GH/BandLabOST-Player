ae879aefaec0a70916f57f6117d6c93d5eb015a8
Joalor64 <rhgarcon@gmail.com>
2023-03-09 11:43:56 -0500

    fixing some formatting cause idk

--- source/states/COFState.hx
+++ source/states/COFState.hx
@@ -16,25 +16,25 @@ class COFState extends FlxState
 
     override function create()
     {
-                openfl.system.System.gc();
+        openfl.system.System.gc();
         
-		super.create();
+	super.create();
 
-                var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('backgrounds/creepyolforest'));
-		bg.scrollFactor.x = 0;
-		bg.scrollFactor.y = 0.18;
-		bg.setGraphicSize(Std.int(bg.width * 1.1));
-		bg.updateHitbox();
-		bg.screenCenter();
-		bg.antialiasing = true;
-		add(bg);
+        var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('backgrounds/creepyolforest'));
+	bg.scrollFactor.x = 0;
+	bg.scrollFactor.y = 0.18;
+	bg.setGraphicSize(Std.int(bg.width * 1.1));
+	bg.updateHitbox();
+	bg.screenCenter();
+	bg.antialiasing = true;
+	add(bg);
 
-                DisplayText = new FlxText(0, 0, FlxG.width, "NOW PLAYING:\nCreepy Ol Forest", 32);
-		DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+        DisplayText = new FlxText(0, 0, FlxG.width, "NOW PLAYING:\nCreepy Ol Forest", 32);
+	DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
 
-		add(DisplayText);
+	add(DisplayText);
 
-                FlxG.sound.playMusic(Paths.music('Creepy_Ol_Forest'), 1);
+        FlxG.sound.playMusic(Paths.music('Creepy_Ol_Forest'), 1);
     }
 
     override public function update(elapsed:Float)
--- source/states/NGState.hx
+++ source/states/NGState.hx
@@ -16,25 +16,25 @@ class NGState extends FlxState
 
     override function create()
     {
-                openfl.system.System.gc();
+        openfl.system.System.gc();
         
-		super.create();
+	super.create();
 
-                var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('backgrounds/nighttimegaming'));
-		bg.scrollFactor.x = 0;
-		bg.scrollFactor.y = 0.18;
-		bg.setGraphicSize(Std.int(bg.width * 1.1));
-		bg.updateHitbox();
-		bg.screenCenter();
-		bg.antialiasing = true;
-		add(bg);
+        var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('backgrounds/nighttimegaming'));
+	bg.scrollFactor.x = 0;
+	bg.scrollFactor.y = 0.18;
+	bg.setGraphicSize(Std.int(bg.width * 1.1));
+	bg.updateHitbox();
+	bg.screenCenter();
+	bg.antialiasing = true;
+	add(bg);
 
-                DisplayText = new FlxText(0, 0, FlxG.width, "NOW PLAYING:\nNighttime Gaming", 32);
-		DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+        DisplayText = new FlxText(0, 0, FlxG.width, "NOW PLAYING:\nNighttime Gaming", 32);
+	DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
 
-		add(DisplayText);
+	add(DisplayText);
 
-                FlxG.sound.playMusic(Paths.music('Nighttime_Gaming'), 1);
+        FlxG.sound.playMusic(Paths.music('Nighttime_Gaming'), 1);
     }
 
     override public function update(elapsed:Float)
