16dc019540bbed96a3d0b1236e850065c70f13cd
Joalor64 <rhgarcon@gmail.com>
2023-03-10 09:35:21 -0500

    yeah so umm more formatting

--- source/states/menu/InstructionsState.hx
+++ source/states/menu/InstructionsState.hx
@@ -15,29 +15,29 @@ class InstructionsState extends FlxState
 
     override function create()
     {
-		openfl.system.System.gc();
+	openfl.system.System.gc();
         
-		super.create();
+	super.create();
 
-                var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
-		bg.scrollFactor.x = 0;
-		bg.scrollFactor.y = 0.18;
-		bg.setGraphicSize(Std.int(bg.width * 1.1));
-		bg.updateHitbox();
-		bg.screenCenter();
-		bg.antialiasing = true;
-		add(bg);
+        var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+	bg.scrollFactor.x = 0;
+	bg.scrollFactor.y = 0.18;
+	bg.setGraphicSize(Std.int(bg.width * 1.1));
+	bg.updateHitbox();
+	bg.screenCenter();
+	bg.antialiasing = true;
+	add(bg);
 
-                DisplayText = new FlxText(0, 0, FlxG.width, 
-				"Use the UP and DOWN keys to" 
-				+ "\nnavigate through the menus."
-				+ "\nChoose a song to listen to it."
-				+ "\nWhen you are done listening,"
-				+ "\npress ESC to go back."
-				+ "\nOtherwise, press BACKSPACE to" 
-				+ "\ngo back to the Song Selector.", 32);
-		DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
-		add(DisplayText);
+        DisplayText = new FlxText(0, 0, FlxG.width, 
+	            "Use the UP and DOWN keys to" 
+		    + "\nnavigate through the menus."
+		    + "\nChoose a song to listen to it."
+		    + "\nWhen you are done listening,"
+		    + "\npress ESC to go back."
+		    + "\nOtherwise, press BACKSPACE to" 
+		    + "\ngo back to the Song Selector.", 32);
+	DisplayText.setFormat(Paths.font("vcr.ttf"), 54, FlxColor.WHITE, FlxTextAlign.CENTER,FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+	add(DisplayText);
     }
 
     override public function update(elapsed:Float)
